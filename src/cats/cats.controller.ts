1eeb7ec2456703e3936e90ed7bae2e82b6bbab36
luongba <luongba@kaopiz.com>
2023-12-22 20:21:05 +0700

    If use library-specific mode

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('cats')
 export class CatsController {
     @Get('breed')
-    findAll(): string {
+    findAll(@Req() request: Request, @Res() response: Response): string {
         return 'This action returns all cats';
     }
     //@Get() tells Nest to create a handler for a specific end point (HTTP request method + route path)
@@ -16,4 +17,26 @@ export class CatsController {
         -> res handling simple: just return the value, Nest takes care of the rest.
     - Library-specific: can be injected using the @Res() decorator in the method handler signature (e.g., findAll(@Res() response))
         -> have the ability to use the native response handling methods exposed by that object (e.g. Express, response.status(200).send())
+*/
+
+/* Dedicated decorators:
+    @Request(), @Req()          req
+    @Response(), @Res()         res
+    @Next()                     next
+    @Session()                  req.session
+    @Param(key?: string)        req.params/req.params[key]
+    @Body(key?: string)         req.body/req.body[key]
+    @Query(key?: string)        req.query/req.query[key]
+    @Headers(name?: string)     req.header/req.header[name]
+    @Ip()                       req.ip
+    @HostParam()                req.hosts
+
+    When inject @Res(),... in method handler -> put Nest into Library-specific mode for that handler -> become responsible for managing the response
+    -> MUST issue some kind of response by making a call on the response object (e.g., res.json(...), res.send(...)) or the HTTP server will HANG.
+*/
+
+/*Nest detects when the handler using library-specific.
+  If both approaches are used at the same time, the Standrad approach is automatically disabled for this single route and no longer work as expected.
+  To use both approaches at the same time (for example, by injecting the res object to only set cookies/headers but still leave the rest to the framework),
+  MUST set like this: @Res({ passthrough: true }) in @Res decorator.
 */
